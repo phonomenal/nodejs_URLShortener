da0cf6ce065e4fc8dafc272db3fbe25c6756647a
James Ha <james-leha@github.com>
2020-04-24 15:19:01 -0500

    add test blank

---
+++ test/test.js
@@ -0,0 +1,11 @@
+/*
+var expect  = require('chai').expect;
+var request = require('request');
+
+it('Main page content', function(done) {
+    request('http://localhost:3000' , function(error, response, body) {
+        expect(body).to.contain('Welcome to Azure!');
+        done();
+    });
+});
+*/
